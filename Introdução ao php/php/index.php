6b111af1049f710df1149305d82bcdd91a7da447
admnando <nando.comth@gmail.com>
2025-09-25 19:22:12 -0300

    Aula 8- Atividade 1

---
+++ Introdução ao php/php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <p>Teste 1</p>
+  </body>
+
+  <?php
+  define("nome", "Fernando"); //constante
+  $nomeVar = "Fernando"; //variável
+  $var1 = 10; //inteiro
+  $var2 = 10.5; //float
+
+  echo "<p>$var1</p>";
+  echo "<p>$nomeVar</p>";  
+  echo gettype($var1);
+  ?>
+
+</html>
