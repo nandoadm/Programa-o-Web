2afc59ef9f8f8ff37a3a6995053a4ebec78e4655
admnando <nando.comth@gmail.com>
2025-10-16 20:25:14 -0300

    Insert e Select php

---
+++ Introdução ao php/php/pratica6.php
@@ -0,0 +1,15 @@
+<?php
+$boletim = ["Disciplina" => "Matemática", "Professor" => "Manu", "Nota1" => 8, "Nota2" => 7.5, "Média" => 7.75];
+
+foreach ($boletim as $key => $value) {
+   echo "<html>";
+    echo "<head><title>Boletim Escolar</title></head>";
+    echo "<body>";
+    echo "<td>";
+
+    echo"</body>";
+    echo "</html>";
+    ;
+   }
+
+?>
---
+++ conexionDb/index.php
@@ -0,0 +1,40 @@
+<?php
+$connectionString = ("host=localhost port=5432 dbname=local user=postgres password=123");
+$connection = pg_connect($connectionString);
+
+if ($connection) {
+    echo "Conectado com sucesso ao banco de dados.";
+
+    $result = pg_query($connection, "SELECT COUNT(*) FROM tbpessoa");
+    if ($result) {
+        $row = pg_fetch_row($result);
+        echo "<br>Número de tabelas no banco de dados: " . $row[0];
+    } else {
+        echo "Erro ao executar a consulta.";
+    }
+
+
+
+    $dadosPessoa = array(
+        "pesnome" => "Joao",
+        "pessobrenome" => "bla",
+        "pesemail" => "bla@gmail.com",
+        "pespassword" => "123",
+        "pescidade" => "Sao Paulo",
+        "pesestado" => "SP"
+    );
+
+    $insert = pg_query_params($connection, "INSERT INTO tbpessoa (pesnome, pessobrenome, pesemail, pespassword, pescidade, pesestado)
+     VALUES ($1, $2, $3, $4, $5, $6)", array_values($dadosPessoa));
+
+    if ($insert) {
+        echo "<br>Dados inseridos com sucesso.";
+        $result = pg_query($connection, "SELECT * FROM tbpessoa");
+    } else {
+        echo "<br>Erro ao inserir dados.";
+    }
+
+    pg_close($connection);
+} else {
+    echo "Conexão Falhou.";
+}
